efd867440422654b2852f8c7909baf0b73775832
Izabella <manansoni.soni77@gmail.com>
2023-04-23 02:21:26 +0530

    Update signup and login routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,4 +1,6 @@
+from datetime import datetime, timedelta
 from flask import current_app as app, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import *
 
 @app.route('/')
@@ -6,18 +8,43 @@ def index_page():
     print('Index Page')
     return jsonify({'msg':'Hello!'})
 
-@app.route('/register/user',methods=['POST'])
-def register_user():
-    print('Register User')
+@app.route('/signup', methods=['POST'])
+def signup():
+    print('Signup')
     data = request.get_json()
     print(data)
     try:
-        user = User(name=data['name'], age=data['age'])
+        user = User(name=data['name'], age=data['age'], username=data['username'], password=data['password'])
         db.session.add(user)
         db.session.commit()
+        token = jwt.encode({'user_id': user.user_id, 'exp': datetime.utcnow()+timedelta(hours=1)},
+                           app.config['SECRET_KEY'], 'HS256')
+        token = token.decode('utf-8')
+        return make_response(jsonify({'token': token}))
     except KeyError as e:
         print('keyerror, ', e)
-    return jsonify('successful')
+    except Exception as e:
+        print('Error, ', e)
+    return 500
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    print('Signin')
+    data = request.get_json()
+    print(data)
+    username = data['username']
+    password = data['password']
+    user = User.query.filter_by(username=username).first()
+    if user:
+        if check_password_hash(user.password, password):
+            token = jwt.encode({'user_id': user.user_id, 'exp': datetime.utcnow()+timedelta(hours=1)},
+                               app.config['SECRET_KEY'], 'HS256')
+            token = token.decode('utf-8')
+            return make_response(jsonify({"message": 'Logged In!', "token": token}), 201)
+        else:
+            return make_response(jsonify({"message": 'Wrong Password!'}), 202)
+    else:
+        return make_response(jsonify({"message": 'User Does Not Exist!'}), 203)
 
 @app.route('/register/event',methods=['POST'])
 def register_event():
@@ -46,6 +73,7 @@ def book_event():
     return jsonify('successful')
 
 @app.route('/list-events', methods=['GET'])
+@token_required
 def get_event():
     print('list events')
     try:
