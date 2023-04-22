567d5c8fee9f60114c2377058a95edde7564dca0
Izabella <manansoni.soni77@gmail.com>
2023-04-23 02:35:53 +0530

    Fix user model

--- backend/app/models.py
+++ backend/app/models.py
@@ -18,8 +18,8 @@ class User(db.Model):
     __tablename__ = 'User'
     user_id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
-    age = db.Column(db.Integer, nullable=True)
-    user_name = db.Column(db.String, primary_key=True, unique=True, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    user_name = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
 
 class Event(db.Model, Serializer):
