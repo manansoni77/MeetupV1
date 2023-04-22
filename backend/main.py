1a4c38ba335812b2f396f1db8786e6245df356ea
Izabella <manansoni.soni77@gmail.com>
2023-04-22 15:01:59 +0530

    Allow cors

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from app.models import db
 from config import *
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = app.config['SECRET_KEY']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(current_dir, DB_NAME)
