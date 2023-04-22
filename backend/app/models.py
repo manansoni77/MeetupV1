91463c0e50ab5ce9cfac76ba3b1f64a168acc949
Izabella <manansoni.soni77@gmail.com>
2023-04-23 02:22:24 +0530

    Update user model with password and token

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import request, current_app as app, make_response, jsonify
+from functools import wraps
+import jwt
 
 db = SQLAlchemy()
 
@@ -16,6 +19,8 @@ class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer, nullable=True)
+    user_name = db.Column(db.String, primary_key=True, unique=True, nullable=False)
+    password = db.Columns(db.String, nullable=False)
 
 class Event(db.Model, Serializer):
     __tablename__ = 'Event'
@@ -28,3 +33,19 @@ class Booking(db.Model):
     booking_id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     event_id = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, nullable=False)
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return make_response(jsonify({"message": "A valid token is missing!"}), 401)
+        try:
+            data = jwt.decode(
+                token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        except Exception:
+            return make_response(jsonify({"message": "Invalid token!"}), 401)
+        return f(data['user_id'], *args, **kwargs)
+    return decorator
