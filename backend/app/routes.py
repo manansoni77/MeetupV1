2b6be5bbcef57777cf6f12f3cb0d44afd7b2297a
Izabella <manansoni.soni77@gmail.com>
2023-05-06 00:32:50 +0530

    Move exception handling to wrapper

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -8,35 +8,43 @@ def devres(result):
     print(result)
     return jsonify(result)
 
-@app.route('/')
+def handle_exceptions(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except KeyError as e:
+            print('Key Error, ', e)
+            return 'Key Error', 500
+        except IntegrityError as e:
+            db.session.rollback()
+            print('Integrity Error, ', e)
+            return 'Integrity Error', 500
+        except Exception as e:
+            print('Error, ', e)
+            return 'Error', 500
+    return decorated
+
+@app.route('/', methods=['GET'])
 def index_page():
     print('Index Page')
     return devres({'msg':'Hello!'})
 
 @app.route('/signup', methods=['POST'])
+@handle_exceptions
 def signup():
     print('Signup')
     data = request.get_json()
     print(data)
-    try:
-        user = User(name=data['name'], age=data['age'], username=data['username'], password=generate_password_hash(data['password']))
-        db.session.add(user)
-        db.session.commit()
-        token = jwt.encode({'user_id': user.user_id, 'exp': datetime.utcnow()+timedelta(hours=1)},
-                           app.config['SECRET_KEY'], 'HS256')
-        return make_response(devres({'token': token}), 200)
-    except KeyError as e:
-        print('Key Error, ', e)
-        return 'Key Error', 500
-    except IntegrityError as e:
-        db.session.rollback()
-        print('Integrity Error, ', e)
-        return 'Integrity Error', 500
-    except Exception as e:
-        print('Error, ', e)
-        return 'Error', 500
+    user = User(name=data['name'], age=data['age'], username=data['username'], password=generate_password_hash(data['password']))
+    db.session.add(user)
+    db.session.commit()
+    token = jwt.encode({'user_id': user.user_id, 'exp': datetime.utcnow()+timedelta(hours=1)},
+                        app.config['SECRET_KEY'], 'HS256')
+    return make_response(devres({'token': token}), 200)
 
 @app.route('/signin', methods=['POST'])
+@handle_exceptions
 def signin():
     print('Signin')
     data = request.get_json()
@@ -56,29 +64,25 @@ def signin():
         return make_response(devres({"message": 'User Does Not Exist!'}), 203)
 
 @app.route('/register/event',methods=['POST'])
+@handle_exceptions
 def register_event():
     print("Register Event")
     data = request.get_json()
     print(data)
-    try:
-        event = Event(title=data['title'], location=data['location'])
-        db.session.add(event)
-        db.session.commit()
-    except KeyError as e:
-        print('keyerror, ', e)
+    event = Event(title=data['title'], location=data['location'])
+    db.session.add(event)
+    db.session.commit()
     return devres('successful')
 
 @app.route('/book', methods=['POST'])
+@handle_exceptions
 def book_event():
     print('book event')
     data = request.get_json()
     print(data)
-    try:
-        booking = Booking(user_id=data['user_id'], event_id=data['event_id'])
-        db.session.add(booking)
-        db.session.commit()
-    except KeyError as e:
-        print('keyerror, ', e)
+    booking = Booking(user_id=data['user_id'], event_id=data['event_id'])
+    db.session.add(booking)
+    db.session.commit()
     return devres('successful')
 
 @app.route('/list-events', methods=['GET'])
