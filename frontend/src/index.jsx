d6de0888cf23b44aa487db1e866d344d601c7b81
Izabella <manansoni.soni77@gmail.com>
2023-05-21 03:18:24 +0530

    Update index file and css

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,46 +5,58 @@ import "./index.css";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
 import { Error } from "./components/Error";
-import { Signup } from "./components/Signup";
+import { Signin, Signup, Signout } from "./components/Auth";
+import { CreateEvent, ListEvents, Register } from "./components/Event";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        // signup route 
-        path: 'signup',
-        element: <Signup />
-      },
-      {
-        // signin route
-        path: 'signin'
-      },
-      {
-        // user settings route
-        path: 'user'
-      },
-      {
-        // create event route
-        path: 'create-event'
-      },
-      {
-        // see current events
-        path: 'events',
-      },
-      {
-        // book place in event
-        path: 'book',
-      }
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <Error />,
+      children: [
+        {
+          path: "about",
+          element: <About />,
+        },
+        {
+          // signup route
+          path: "signup",
+          element: <Signup />,
+        },
+        {
+          // signin route
+          path: "signin",
+          element: <Signin />,
+        },
+        {
+          // signout route
+          path: "signout",
+          element: <Signout />
+        },
+        {
+          // user settings route
+          path: "user",
+        },
+        {
+          // create event route
+          path: "create-event",
+          element: <CreateEvent />,
+        },
+        {
+          // see current events
+          path: "events",
+          element: <ListEvents />
+        },
+        {
+          // register for event
+          path: "register/:event_id",
+          element: <Register />
+        },
+      ],
+    },
+  ],
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
